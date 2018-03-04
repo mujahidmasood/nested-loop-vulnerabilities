1d910998a692f0853f4ee73cc0651d39661876b8
Mujahid Masood <mujahid.masood@stud.tu-darmstadt.de>
2018-03-04 17:14:19 +0100

    - code cleanup - added comments

--- src/main/java/com/ptaa/code/AnalyzerClient.java
+++ src/main/java/com/ptaa/code/AnalyzerClient.java
@@ -8,16 +8,19 @@ import java.nio.file.Paths;
  */
 public class AnalyzerClient {
     public static void main(String[] args) {
-        String modules_path = "/home/mujahidmasood/Masters/DSS/Semester5/PTAA/nested-loop-vulnerabilities/node_modules";
+        String modules_path = "src/test/resources";
+        //String modules_path = "/home/mujahidmasood/Masters/DSS/Semester5/PTAA/nested-loop-vulnerabilities/node_modules";
         try {
             //Walking through given directory
             //identifying non hidden files
-            //filter files with js extension
+            //filter files with js or ts extension
             //read every file to find out vulnerabilities
             Files.walk(Paths.get(modules_path))
                     .filter(Files::isRegularFile)
                     .filter(path -> !Files.isDirectory(path))
-                    .filter(file -> file.toString().endsWith(".js"))
+                    .filter(file ->
+                        file.toString().endsWith(".js") || file.toString().endsWith(".ts")
+                    )
                     .forEach(path -> {
                         try {
                             System.out.println(path.toString());
